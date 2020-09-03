bd8a22fcb0fd508107cbcfbc3133ccd6928cd048
Iulian Beleiu <i.beleiu@plan-net.com>
2020-09-03 09:21:26 +0300

    markdown service

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -26,17 +27,24 @@ class QuestionController extends AbstractController
 
     /**
      * @Route("/questions/{slug}", name="app_question_show")
+     * @param $slug
+     * @param MarkdownParserInterface $markdownParser
+     * @return Response
      */
-    public function show($slug)
+    public function show($slug, MarkdownParserInterface $markdownParser)
     {
         $answers = [
-            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Make sure your cat is sitting `purrrfectly` still 🤣',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
 
+        $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+        $parsedQuestionText = $markdownParser->transformMarkdown($questionText);
+
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
+            'questionText' => $parsedQuestionText,
             'answers' => $answers,
         ]);
     }
